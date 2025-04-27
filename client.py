a0e7faa2641972b15f271712422602c3fd68fe0c
Hemant-coder99 <hemantbahubali123@gmail.com>
2025-04-27 07:40:57 +0545

    api is removed

--- client.py
+++ client.py
@@ -1,20 +1,3 @@
-# from openai import OpenAI
-
-# client = OpenAI(
-#   api_key="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"
-# )
-# completion = client.chat.completions.create(
-#       model="gpt-4o-mini",
-#       messages=[
-#           {"role": "system", "content": "You are a virtual assistant name jarvis."},
-#           {
-#               "role": "user",
-#               "content": "Write an essay in money."
-#           }
-#       ]
-#   )
-
-# print(completion.choices[0].message)
 from openai import OpenAI
 
 client = OpenAI(
